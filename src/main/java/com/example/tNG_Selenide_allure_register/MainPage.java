6272c05fa731b55919984b5a3455a9b9183e1df5
ivan Ba <ipbabs@ya.ru>
2021-02-11 00:00:32 +0300

    some task

--- src/main/java/com/example/tNG_Selenide_allure_register/MainPage.java
+++ src/main/java/com/example/tNG_Selenide_allure_register/MainPage.java
@@ -1,8 +1,10 @@
 package com.example.tNG_Selenide_allure_register;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 
 public class MainPage {
@@ -10,5 +12,8 @@ public class MainPage {
 
     public SelenideElement seeAllToolsButton = $(By.linkText("Спорт и отдых"));
     public SelenideElement toolsMenu = $(By.linkText("Игрушки и игры"));
-    public SelenideElement searchButton = $(By.className("fu"));
+    public SelenideElement searchButton = $(By.className("PG"));
+    public MainPage open(){
+        Selenide.open("/");
+        return this;}
 }
--- src/test/java/com/example/tNG_Selenide_allure_register/MainPageTest.java
+++ src/test/java/com/example/tNG_Selenide_allure_register/MainPageTest.java
@@ -1,27 +1,79 @@
 package com.example.tNG_Selenide_allure_register;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Driver;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.openqa.selenium.By;
-import org.testng.annotations.*;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.driver;
+import static org.openqa.selenium.Keys.ENTER;
 
 public class MainPageTest {
     private final MainPage mainPage = new MainPage();
 
     @BeforeClass
-    public static void setUp() {
+    public   void setUp() throws MalformedURLException {
 
         SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.startMaximized = true;
-        open("https://www.detmir.ru/");
+
+        Configuration.baseUrl = "https://www.detmir.ru";
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        Map<String, Object> profile = new HashMap<String, Object>();
+        Map<String, Object> contentSettings = new HashMap<String, Object>();
+        contentSettings.put("notifications", 2);
+        contentSettings.put("geolocation", 2);
+        profile.put("managed_default_content_settings", contentSettings);
+
+        prefs.put("profile", profile);
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-plugins");
+        options.addArguments("--start-maximized");
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "88.0");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "7");
+        caps.setCapability("resolution", "1280x1024");
+        caps.setCapability("browserstack.debug", "true");
+        caps.setCapability(ChromeOptions.CAPABILITY, options);
+        WebDriver driver = new ChromeDriver(options);
+        WebDriverRunner.setWebDriver(driver);
+        open("/");
+
+
 
     }
-
 
 
     @Test(priority = 1, enabled = true)
@@ -32,10 +84,11 @@ public class MainPageTest {
          * click button
          * checking  result search
          */
-         mainPage.searchButton.click();
-         $(By.className("l_5")).sendKeys("Игрушки");
-        $(byClassName("Wq")).click();//кнопка поиска
-        $(byClassName("bb")).shouldHave(exactText( "«Игрушки»"));
+       // mainPage.open();
+
+         mainPage.searchButton.shouldBe(visible).click();
+        $(By.xpath("//input[@type='search']")).sendKeys("Игрушки", ENTER);
+        $(byClassName("bu")).shouldHave(exactText( "«Игрушки»"));
     }
 
     @Test(priority = 2, enabled = true)
@@ -60,4 +113,5 @@ public class MainPageTest {
 
 
     }
+
 }
